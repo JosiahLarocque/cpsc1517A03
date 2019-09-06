d0438ce9822ccadef30919a5a79aac09e5d16486
JosiahLarocque <46633994+JosiahLarocque@users.noreply.github.com>
2019-09-06 11:43:23 -0600

    create FencePanel object for OOPs project

---
+++ OOPsSolution/OOPsReview/FencePanel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsReview
+{
+    //by default all classes are private
+    //for this class to be used by an outside user
+    //   you must alter the permission for this class
+    //   you will use public 
+    public class FencePanel
+    {
+    }
+}
