c8eb39dec64c01282048788238841d92b2f7adc5
JosiahLarocque <46633994+JosiahLarocque@users.noreply.github.com>
2019-09-13 10:09:40 -0600

    Update Gate.cs

--- OOPsSolution/OOPsReview/Gate.cs
+++ OOPsSolution/OOPsReview/Gate.cs
@@ -6,10 +6,9 @@ using System.Threading.Tasks;
 
 namespace OOPsReview
 {
-    class Gate
+    public class Gate
     {
         private double _Height;
-
         public double Height
         {
             get
@@ -18,34 +17,62 @@ namespace OOPsReview
             }
             set
             {
-                if (value > 0 && value <= 8.0)
+                if (value > 0.0 && value <= 8.0)
                 {
                     _Height = value;
                 }
                 else
                 {
-                    throw new Exception("Invalid height");
+                    throw new Exception("Invalid heigth.");
                 }
             }
         }
 
+        //could fully implement like Height
         public double Width { get; set; }
 
-        public double Price { get; set; }
-
         private string _Style;
-        public Gate(double height, double width, string style, double price)
+
+        public string Style
         {
-            double Height = height;
-            double Width = width;
-            string Style = style;
-            double Price = price;
+            get
+            {
+                return _Style;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    _Style = null;
+                }
+                else
+                {
+                    _Style = value;
+                }//eof
+            }
         }
 
+        //could fully implement like Height
+        public double Price { get; set; }
+
+        public Gate()
+        {
+            Height = 6.0;
+            Width = 4.0;
+        }
+
+        public Gate(double height, double width, string style, double price)
+        {
+            Height = height;
+            Width = width;
+            Style = style;
+            Price = price;
+        }
 
         public double GateArea()
         {
-            //probably should check that width and height has value
+            //probably should first check that width and height
+            //   have values
             return Width * Height;
         }
     }
