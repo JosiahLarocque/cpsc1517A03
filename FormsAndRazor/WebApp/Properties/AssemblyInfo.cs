51ffbaf0133f89b6f9f3d97329a82b994ab2c2eb
JosiahLarocque <46633994+JosiahLarocque@users.noreply.github.com>
2019-10-21 00:34:42 -0600

    .

--- FormsAndRazor/WebApp/Properties/AssemblyInfo.cs
+++ FormsAndRazor/WebApp/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e1f9dcd6-cccf-4b6a-bc33-ae81afd0a0b0")]
+[assembly: Guid("d3d925d1-6fc5-4762-b842-d846d6c0c340")]
 
 // Version information for an assembly consists of the following four values:
 //
