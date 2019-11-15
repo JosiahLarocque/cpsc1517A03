5ae6d86f12bdd5dfd099583c6a4000df87274570
JosiahLarocque <46633994+JosiahLarocque@users.noreply.github.com>
2019-11-15 11:37:48 -0700

    m,

---
+++ CSNet/NorthWindSystem/BLL/CategoryController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using NorthwindSystem.Data;
+using NorthwindSystem.DAL;
+#endregion
+
+namespace NorthwindSystem.BLL
+{
+    public class CategoryController
+    {
+        public List<Category> Categories_List()
+        {
+            using (var context = new NorthwindContext())
+            {
+                //transaction code
+                return context.Categories.ToList();
+            }
+
+        }
+
+        //method that will return a specific region record based on its pkey
+        public Category Categories_FindByID(int CategoryID)
+        {
+            //connect to the context class that will handle the data request
+            //most of CRUD requires using a transaction
+            //To ensure that your data request is handled as a transaction
+            //   we will encase all controller action within a transaction
+            using (var context = new NorthwindContext())
+            {
+                //transaction code
+                return context.Categories.Find(CategoryID);
+            }
+
+        }
+    }
+}
